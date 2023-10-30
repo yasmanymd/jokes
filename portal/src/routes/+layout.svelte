361057c7f44ea9c2a688c71b80ee2120cbc933e0
yasmanymd <yasmany@gmail.com>
2023-10-29 22:44:02 -0400

    included sveltekit

---
+++ portal/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/random">random</a>
+</nav>
+
+<slot />
---
+++ portal/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<section>
+	<div style="display: flex;">
+		<div class="card" style="width: 80%;" />
+		<div class="card" style="width: 20%;">Prueba</div>
+	</div>
+</section>
+
+<style>
+	.card {
+		margin: 10px;
+		padding: 10px;
+		border-radius: 10px;
+		background-color: cornflowerblue;
+	}
+</style>
